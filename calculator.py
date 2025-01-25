8c48960b14338966e59710c54c3bc954db1a6d6c
Elena-eli05 <elenaevana20052@gmail.com>
2025-01-25 21:33:39 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,43 @@
+n=int(input("Enter how many numbers need to perform the operation"))
+print("Choose an option")
+print("Addition")
+print("Subtraction")
+print("Multiplication")
+print("Division")
+
+choice=int(input("Enter your choice, [1/2/3/4]"))
+if choice==1
+  sum=0
+  for i in range(1, n+1):
+    num=int(input("Enter the number"))
+    sum=sum+num
+  i+=1
+  print(sum)
+elif choice==2
+   num=int(input("Enter the number"))
+   sub=num
+   for i in range(1, n):
+     num= int(input("Enter the number"))
+     sub-=num
+  print(sub)
+elif choice==3
+  mult=1
+  for i in range(1, n+1):
+     num= int(input("Enter the number"))
+     mult*=num
+  print(mult)
+else
+   num= int(input("Enter the number"))
+   div=num
+   for i in range(1, n):
+      num= int(input("Enter the number"))
+      div/=num
+   print(div)
+  
+  
+
+ 
+    
+    
+
+  
